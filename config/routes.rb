1ddd390785b5e338341ef54a591e1c4245454be6
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-07-12 00:31:37 +0000

    [start]0712

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'homes#top'
+  resources :users, only: [:show, :edit, :update, :index]
+  resources :books, only: [:show, :index, :new, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
