7e39d63a90a28e0d900a95dacf8aee11fb076c2d
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-07-13 08:55:44 +0000

    rest

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,45 @@
 class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
+    
+    @new_book = Book.new
   end
   
   def index
     @books = Book.all
     
-    @book = Book.new
+    @new_book = Book.new
   end
   
   def create
-    @book = Book.new(books_params)
-    @book.user_id = current_user.id
-    if @book.save
+    @new_book = Book.new(books_params)
+    @new_book.user_id = current_user.id
+    if @new_book.save
       flash[:notice] = "You have created book successfully."
-      redirect_to book_path(@book.id)
+      redirect_to book_path(@new_book.id)
     else
       @books = Book.all
       render :index
     end
   end
   
+  def edit
+    @book = Book.find(params[:id])
+    @new_book = Book.new
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    if book.update(books_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    end
+  end
+  
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   private
