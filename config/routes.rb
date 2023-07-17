d58453b0d23d593d516d1425bb963a686f975c7c
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-07-17 06:58:39 +0000

    [finish]add_iine

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get '/home/about' => 'homes#about', as: 'about'
   resources :users, only: [:show, :edit, :update, :index]
-  resources :books, only: [:show, :index, :new, :create, :destroy, :update, :edit]
+  resources :books, only: [:show, :index, :new, :create, :destroy, :update, :edit] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
