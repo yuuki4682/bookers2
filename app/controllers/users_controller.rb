54ad7722b377264cda622b03130ca51a3d128193
yuuki4682 <132807007+yuuki4682@users.noreply.github.com>
2023-07-27 07:41:45 +0000

    before rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :ensure_guest_user, only: [:edit]
   
   def show
     @user = User.find(params[:id])
@@ -34,4 +35,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.guest_user?
+      redirect_to user_path(current_user), notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,17 @@ class User < ApplicationRecord
   validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
   
+  GUEST_USER_EMAIL = "guest@example.com"
+   def self.guest
+     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
+       user.password = SecureRandom.urlsafe_base64
+       user.name = "guestuser"
+     end
+   end
+  
+  def guest_user?
+    email == GUEST_USER_EMAIL
+  end
   
   def get_profile_image(width, height)
     unless profile_image.attached?
